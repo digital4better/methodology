75a4075beac0c0651d4e52f249d560a4ec601b2e
Stefan <stefan.cosquer@gmail.com>
2024-06-19 17:20:52 +0200

    Changed base URL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ export default {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/methodology',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
